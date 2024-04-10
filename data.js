3db95cdeba5c4b66f103c4f49edc975f15b2e11f
garvsl <garvsehgal7@gmail.com>
2024-04-09 22:16:26 -0400

    added data

---
+++ data.js
@@ -0,0 +1,228 @@
+const books = [
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    genre: "Classic",
+    year: 1960,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/7/79/To_Kill_a_Mockingbird_by_Harper_Lee.jpg",
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Classic",
+    year: 1925,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/f/f7/The_Great_Gatsby_1925_Jacket_Cover.jpg",
+  },
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    genre: "Fantasy",
+    year: 1997,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/b/bf/Harry_Potter_and_the_Sorcerer's_Stone.jpg",
+  },
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    year: 1937,
+    coverURL: "https://upload.wikimedia.org/wikipedia/en/3/30/Hobbit_cover.JPG",
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    genre: "Classic",
+    year: 1813,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/commons/1/16/Pride_and_Prejudice_--_Austen_--_Title_Page.jpg",
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    genre: "Dystopian",
+    year: 1949,
+    coverURL: "https://upload.wikimedia.org/wikipedia/en/c/c3/1984first.jpg",
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    genre: "Modern",
+    year: 1951,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/3/32/CatcherInTheRye.JPG",
+  },
+  {
+    title: "The Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    year: 1954,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/e/e9/First_Single_Volume_Edition_of_The_Lord_of_the_Rings.gif",
+  },
+  {
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    genre: "Fantasy",
+    year: 1988,
+    coverURL: "https://upload.wikimedia.org/wikipedia/en/c/c4/TheAlchemist.jpg",
+  },
+  {
+    title: "The Chronicles of Narnia",
+    author: "C.S. Lewis",
+    genre: "Fantasy",
+    year: 1950,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/c/cb/TheMagician%27sNephew.JPG",
+  },
+  {
+    title: "The Picture of Dorian Gray",
+    author: "Oscar Wilde",
+    genre: "Gothic",
+    year: 1890,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/2/2b/Dorian_Gray_1891_cover.jpg",
+  },
+  {
+    title: "The Hunger Games",
+    author: "Suzanne Collins",
+    genre: "Dystopian",
+    year: 2008,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/d/dc/The_Hunger_Games.jpg",
+  },
+  {
+    title: "The Road",
+    author: "Cormac McCarthy",
+    genre: "Post-apocalyptic",
+    year: 2006,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/2/2c/The_Road_-_book_cover.jpg",
+  },
+  {
+    title: "The Girl with the Dragon Tattoo",
+    author: "Stieg Larsson",
+    genre: "Thriller",
+    year: 2005,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/d/db/Millennium_Swedish_hardcover.jpg",
+  },
+  {
+    title: "I Shall Seal the Heavens",
+    author: "Er Gen",
+    genre: "Cultivation",
+    year: 2014,
+    coverURL:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1471524186l/30351856._SY475_.jpg",
+  },
+  {
+    title: "Renegade Immortal",
+    author: "Er Gen",
+    genre: "Cultivation",
+    year: 2007,
+    coverURL:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1386921253l/18622183.jpg",
+  },
+  {
+    title: "A Will Eternal",
+    author: "Er Gen",
+    genre: "Cultivation",
+    year: 2016,
+    coverURL:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1490101910l/34328959._SY475_.jpg",
+  },
+  {
+    title: "The Grandmaster Strategist",
+    author: "Zhao Zhan",
+    genre: "Cultivation",
+    year: 2008,
+    coverURL:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1491398397l/34850122.jpg",
+  },
+  {
+    title: "The King's Avatar",
+    author: "Butterfly Blue",
+    genre: "Cultivation",
+    year: 2011,
+    coverURL:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1438846321l/29022815.jpg",
+  },
+  {
+    title: "The Da Vinci Code",
+    author: "Dan Brown",
+    genre: "Mystery",
+    year: 2003,
+    coverURL: "https://upload.wikimedia.org/wikipedia/en/6/6b/DaVinciCode.jpg",
+  },
+  {
+    title: "The Shining",
+    author: "Stephen King",
+    genre: "Horror",
+    year: 1977,
+    coverURL: "https://upload.wikimedia.org/wikipedia/en/4/4c/Shiningnovel.jpg",
+  },
+  {
+    title: "Gone Girl",
+    author: "Gillian Flynn",
+    genre: "Thriller",
+    year: 2012,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/6/6b/Gone_Girl_book_cover.jpg",
+  },
+  {
+    title: "The Help",
+    author: "Kathryn Stockett",
+    genre: "Historical Fiction",
+    year: 2009,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/6/62/The_Help_%28novel%29.jpg",
+  },
+  {
+    title: "The Martian",
+    author: "Andy Weir",
+    genre: "Science Fiction",
+    year: 2011,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/c/c3/The_Martian_2014.jpg",
+  },
+  {
+    title: "Outliers: The Story of Success",
+    author: "Malcolm Gladwell",
+    genre: "Non-Fiction",
+    year: 2008,
+    coverURL: "https://upload.wikimedia.org/wikipedia/en/1/18/Outliers.png",
+  },
+  {
+    title: "Sapiens: A Brief History of Humankind",
+    author: "Yuval Noah Harari",
+    genre: "Non-Fiction",
+    year: 2011,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/5/5e/Sapiens_A_Brief_History_of_Humankind.jpg",
+  },
+  {
+    title: "The Power of Habit: Why We Do What We Do in Life and Business",
+    author: "Charles Duhigg",
+    genre: "Non-Fiction",
+    year: 2012,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/9/96/The_Power_of_Habit.jpg",
+  },
+  {
+    title: "Educated",
+    author: "Tara Westover",
+    genre: "Non-Fiction",
+    year: 2018,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/1/1c/Educated_by_Tara_Westover_book_cover.jpg",
+  },
+  {
+    title: "The Immortal Life of Henrietta Lacks",
+    author: "Rebecca Skloot",
+    genre: "Non-Fiction",
+    year: 2010,
+    coverURL:
+      "https://upload.wikimedia.org/wikipedia/en/7/70/The_Immortal_Life_of_Henrietta_Lacks_cover.jpg",
+  },
+];
